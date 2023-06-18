b136df6110e5848a1e0c6d0b86b577eac6df0cdc
Bhaktij Koli <bhaktijkoli121@gmail.com>
2023-06-18 13:24:50 +0530

    chore: init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the simple express demo!')
+})
+
+app.get('/ping', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
